faa58515591b7ababb1bfad3ef1a958578763543
Danielle Crosswell <Danielle@dyn-160-39-150-144.dyn.columbia.edu>
2016-02-06 05:24:43 -0500

    added mongodb cloud functionality

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
 import urllib
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 url = 'https://www.google.com/search?q=meme&safe=off&biw=1279&bih=617&source=lnms&tbm=isch&sa=X&'
 response = requests.get(url)
@@ -11,9 +12,13 @@ soup = BeautifulSoup(html, "html.parser")
 images = soup.find('table', attrs={'class': 'images_table'})
 
 memes = images.findAll('td')
-i = 0
+client = MongoClient('dyn-160-39-150-144.dyn.columbia.edu', 27000)
+db = client['memes']
 for row in memes:
 	if row.find('a') is not None:
 		memeUrl = row.find('img').get('src')
-		urllib.urlretrieve(memeUrl, 'meme' + str(i) + '.jpg')
-		i += 1
+		db.memes.insert_one( {
+			"_id": memeUrl
+			}
+		)
+
